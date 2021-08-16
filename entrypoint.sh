27f8c3e09b058d4dcfed66258b07cd7a806b2c9f
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2021-08-16 15:57:40 +0200

    fix: avoid deleting all pages

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,11 @@ CONFLUENCE_SERVER=${CONFLUENCE_SERVER:-}
 CONFLUENCE_USER=${CONFLUENCE_USER:-}
 CONFLUENCE_PASS=${CONFLUENCE_PASS:-}
 
-sphinx-build -b ${SPHINX_BUILDER} -a -E "${SOURCEDIR}" "${SOURCEDIR}/_build" \
+sphinx-build -b ${SPHINX_BUILDER} "${SOURCEDIR}" "${SOURCEDIR}/_build" \
   -D plantuml='/usr/bin/java -jar /usr/local/plantuml.jar' \
   -D confluence_server_url=$CONFLUENCE_SERVER \
   -D confluence_server_user=$CONFLUENCE_USER \
   -D confluence_server_pass=$CONFLUENCE_PASS \
+  -D confluence_purge=True \
+  -D confluence_purge_from_master=True \
   ${SPHINX_BUILD_ARGS} "$@"
